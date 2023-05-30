cd87a39227883181297e0f5d575a81e264c067cf
Navneet <bosshero680@gmail.com>
2023-05-30 16:01:45 +0530

    sidebar path

--- playground/src/components/sidebar.tsx
+++ playground/src/components/sidebar.tsx
@@ -1,8 +1,10 @@
 import React from "react";
-import contractName from "../../src/contracts.json";
+// import contractName from "../../src/contracts.json";
 import ConnectWalletButton from "./common/buttons/connectWallet";
+import ContrctNameJson from "../contracts/instantiateInfo/contractList.json"
 import "./sidebar.css";
 function SideNavbar(): JSX.Element {
+  const contractName = Object.keys(ContrctNameJson);
   return (
     <div className="sidebar">
       <ConnectWalletButton></ConnectWalletButton>
